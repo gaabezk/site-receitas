9cc254bb549a87358dc9d7235b203e8b4d5e4e21
Vinicius da Conceição Teixeira <viniciuscteixeira.rj@gmail.com>
2022-05-11 03:54:03 -0300

    fix

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+var app;
+$(function () {
+    app = {
+        init: function () {
+            app.setEvents();
+        },
+        setEvents: function () {
+            app.loadElements();
+
+            //- disable link with #
+            $('a[href="#"]').click(function (e){
+                e.preventDefault();
+            });
+            //- --------------------------------------------------------------------------------------------------------
+
+            //- load pages
+            $('.loadPage').click(function (e) {
+                e.preventDefault();
+                $('.main').load($(this).data('page'));
+            });
+            //- --------------------------------------------------------------------------------------------------------
+        },
+        loadElements: function () {
+            $('head').load($('head').data('element'));
+            $('nav').load($('nav').data('element'));
+            $('footer').load($('footer').data('element'));
+        },
+        pageLoading : function (show){
+            if (show) {
+                $('#preloaderPage').removeClass('hide');
+            } else {
+                $('#preloaderPage').addClass('hide');
+            }
+        },
+    };
+    app.init();
+});
